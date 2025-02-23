84f320f475783497929932386c859920750e41d6
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-02-23 19:54:35 +0900

    [level 2] Title: 올바른 괄호, Time: 19.60 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 70.3 MB, 시간: 24.64 ms
+메모리: 54.8 MB, 시간: 19.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 18일 11:18:53
+2025년 02월 23일 19:54:33
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,15 +5,18 @@ class Solution {
         boolean answer = true;
         Stack<Character> stack = new Stack<>();
         
+        //Stack에 '('은 저장, ')'은 '('가 있는 경우 pop
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)=='(') 
                 stack.push(s.charAt(i));
             else{
+                //')'는 있는데 스택이 비어있으면 올바른 괄호 X
                 if(stack.empty()) return false;
                 else stack.pop();
             }
         }
         
+        //Stack이 비어있으면 올바른 괄호 X 
         if(!stack.empty()) return false;
 
         return answer;
