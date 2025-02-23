1c468bcf9b4c5b9034c9ebef99ac6795822e4343
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-02-23 19:44:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,36 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
+        int[] answer = {0,0};
+        
         List<String> list = new ArrayList<>();
         list.add(words[0]);
         
-        int idx = 0;
-        for(int i=0; i<words.length-1; i++){
-            char fir = words[i].charAt(words[i].length()-1);
-            char sec = words[i+1].charAt(0);
-            
-            if(list.contains(words[i+1])){
-                idx = i+2;
+        for(int i=1; i<words.length; i++){
+            if(list.contains(words[i])){
+                answer[0] = (i)%n+1;
+                answer[1] = (i)/n+1;
                 break;
             }else{
-                list.add(words[i+1]);
+                list.add(words[i]);
             }
             
+            char fir = words[i-1].charAt(words[i-1].length()-1);
+            char sec = words[i].charAt(0);
             if(fir!=sec){
-                idx = i+2;
+                answer[0] = i%n+1;
+                answer[1] = i/n+1;
                 break;
             }
         }
 
-        if(idx == 0){
-            answer[0] = 0; 
-            answer[1] = 0; 
-        }else{ 
-            answer[1] = (idx-1)/n+1; 
-            answer[0] = idx-(answer[1]-1)*n; 
-        }
-        
         return answer;
     }
 }
