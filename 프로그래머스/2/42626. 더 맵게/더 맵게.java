c878c4229b11dec8ddb03d936134f22cfb40d706
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-02-23 19:57:18 +0900

    [level 2] Title: 더 맵게, Time: 1407.75 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,13 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        //가장 작은 값부터 우선적으로 반환되는 PriorityQueue 사용
         PriorityQueue<Integer> q = new PriorityQueue<>();
         
-        //초기화
         for(int i=0; i<scoville.length; i++) q.offer(scoville[i]);
         
+        //큐의 가장 작은 값이 K보다 커질 때까지 fir+sec*2 과정 반복 
         while(!q.isEmpty()){
             int fir = q.poll();
+            //만약 큐에 값 하나만 있는데 그 값이 K보다 작은 경우 
+            //절대 조건을 만족할 수 없다고 판단, -1 반환 
             if(q.isEmpty() && fir < K) return -1;
             if(fir>=K) break;
             answer ++;
