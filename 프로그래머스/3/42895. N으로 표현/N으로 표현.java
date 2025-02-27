c298f57885cd26f5998d3d09a37fe1c417a738ed
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-02-27 12:52:48 +0900

    [level 3] Title: N으로 표현, Time: 10.32 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -12,20 +12,21 @@ class Solution {
             else{
                 for(int j=0; j<i; j++) res += strN;
                 int resInt = Integer.parseInt(res);
+                //만약 연속되는 숫자 중 정답이 있을 경우 정답 반환 
                 if(resInt==number) return i;
                 Set<Integer> set = new HashSet<>(Arrays.asList(resInt));
                 list.add(set);
             }
         }
         
+        //필요한 N의 개수를 기반으로 memoization! 
         for(int i=2; i<=8; i++){
-
             Set<Integer> set = list.get(i);
             
             for(int j=1; j<=i/2+1; j++){
+                //계산할 N의 개수보다 작은 두 set의 모든 경우의 수를 계산 
                 Set<Integer> set1 = list.get(j);
                 Set<Integer> set2 = list.get(i-j);
-                
                 for(int s1 : set1){
                     for(int s2 : set2){
                         int sum = s1+s2;
@@ -34,12 +35,13 @@ class Solution {
                         set.add(sub);
                         int mul = s1*s2;
                         set.add(mul);
+                        int div = 0;
                         if(s1!=0 && s2!=0){
-                            int div = (s1 > s2) ? s1/s2 : s2/s1;
+                            div = (s1 > s2) ? s1/s2 : s2/s1;
                             set.add(div);
-                            if(div==number) return i;
                         }
-                        if(sum==number||sub==number||mul==number)
+                        //사칙연산 결과 중 정답이 있을 경우 정답을 반환 
+                        if(sum==number||sub==number||mul==number||div==number)
                             return i;
                     }
                 }
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
 
 ### 성능 요약
 
-메모리: 88.9 MB, 시간: 17.23 ms
+메모리: 84.8 MB, 시간: 10.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 12:48:30
+2025년 02월 27일 12:52:46
 
 ### 문제 설명
 
