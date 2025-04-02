571b0f46961d1266d4a327d01af0bc2a034e59f2
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-04-02 21:14:23 +0900

    [level 2] Title: 땅따먹기, Time: 18.95 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,21 @@
+//1차 시도 실패 : DFS로 풀었다가 ALL 시간초과 
+//2차 시도 : DP
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        int[][] dp = new int[land.length][4];
+        for(int i=0; i<4; i++) dp[0][i] = land[0][i];
+        
+        for(int i=1; i<land.length; i++){
+            for(int j=0; j<4; j++){
+                for(int k=0; k<4; k++){
+                    if(j!=k)
+                        dp[i][j] = Math.max(dp[i][j], land[i][j] + dp[i-1][k]);
+                }
+            }
+        }
+        
+        for(int i=0; i<4; i++) answer = Math.max(answer, dp[land.length-1][i]);
+        return answer;
+    }
+}
