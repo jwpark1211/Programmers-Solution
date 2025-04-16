97a7b8b23dacc0e0758eb5d0bd14a60a30953e3d
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-04-16 11:32:24 +0900

    [level 2] Title: 전화번호 목록, Time: 367.14 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 100 MB, 시간: 377.99 ms
+메모리: 98.5 MB, 시간: 367.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 06일 13:38:38
+2025년 04월 16일 11:32:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,18 +3,13 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        //정렬
-        Arrays.sort(phone_book);
         
-        //phone_book[i]와 phone_book[i+1] 비교 (phone_book[i]가 prefix)
-        for(int i=0; i<phone_book.length-1;i++){
-            String prefix = phone_book[i];
-            String phoneNumber = phone_book[i+1];
-            
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length-1; i++){
             boolean flag = true;
-            for(int j=0; j<prefix.length(); j++){
-                if(prefix.charAt(j)!=phoneNumber.charAt(j)){
-                    flag = false; 
+            for(int j=0; j<phone_book[i].length(); j++){
+                if(phone_book[i].charAt(j)!=phone_book[i+1].charAt(j)){
+                    flag = false;
                     break;
                 }
             }
