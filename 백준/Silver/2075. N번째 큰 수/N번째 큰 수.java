b44f30c76e1223a21ec9f9dc99fda911923e19ce
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-04-16 10:22:02 +0900

    [Silver III] Title: N번째 큰 수, Time: 2952 ms, Memory: 387980 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+//시도 1 : pq로만 푸니까 시간 초과
+public class Main {
+    static int N;
+    public static void main(String[] args) {
+        //초기화
+        Scanner scanner = new Scanner(System.in);
+        PriorityQueue<Long> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        N = scanner.nextInt();
+        for(int i=0; i<N; i++){
+            scanner.nextLine();
+            for(int j=0; j<N; j++){
+                pq.add(scanner.nextLong());
+            }
+        }
+
+        for(int i=0; i<N-1; i++){
+            pq.remove();
+        }
+        System.out.println(pq.poll());
+    }
+}
