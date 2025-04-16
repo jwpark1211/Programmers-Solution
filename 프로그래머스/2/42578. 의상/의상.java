d488a87c1e768bcd56d567bf0a5e026f23c2969d
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-04-16 11:43:01 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String,Integer> hmap = new HashMap<>();
+        Map<String, Integer> hm = new HashMap<>();
+        for(String[] ct : clothes){
+            hm.put(ct[1], hm.getOrDefault(ct[1],0)+1);
+        }
         
-        for (String[] tmp : clothes) {
-            hmap.put(tmp[1], hmap.getOrDefault(tmp[1], 0) + 1);
+        int sum = 1;
+        Set<String> key = hm.keySet();
+        for(String k : key){
+           sum *= (hm.get(k)+1);
         }
-            
-        Iterator<String> iterator = hmap.keySet().iterator();
-        while(iterator.hasNext()){
-            answer *= (hmap.get(iterator.next())+1);
-        }
-
-        return --answer;
+        
+        return sum-1;
     }
 }
