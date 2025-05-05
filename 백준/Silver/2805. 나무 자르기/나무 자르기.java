628ddf6154d269d6c68d4241331f0682958d4cbd
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-05-05 13:05:00 +0900

    [Silver II] Title: 나무 자르기, Time: 1020 ms, Memory: 125580 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static long M;
+
+    public static void main(String[] args) throws IOException {
+        long answer = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Long.parseLong(st.nextToken());
+        long[] arr = new long[N];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) arr[i] = Long.parseLong(st2.nextToken());
+        Arrays.sort(arr);
+
+        long low = 0;
+        long high = arr[N-1];
+        while(low<=high){
+            long mid = (low+high)/2;
+            //System.out.println("mid = " + mid);
+
+            long sum = 0;
+            for(int i=N-1; i>=0; i--){
+                //System.out.println("mid = " + mid);
+               if(arr[i]<=mid) break;
+               sum += arr[i] - mid;
+            }
+            //System.out.println("sum = " + sum);
+
+            if(sum<M){
+                high = mid - 1;
+            }else {
+                answer = mid;
+                low = mid + 1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
