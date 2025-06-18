656b3c6bb3058ae19811e45c2ba8ca261808b380
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-06-18 11:20:01 +0900

    [Silver I] Title: 경로 찾기, Time: 296 ms, Memory: 21492 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -3,42 +3,32 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static int N;
-    public static int[][] arr, res;
+    public static int arr[][];
 
-    public static void dfs(int start, int current, boolean[] visited) {
-        for (int next = 0; next < N; next++) {
-            if (arr[current][next] == 1 && !visited[next]) {
-                visited[next] = true;
-                res[start][next] = 1;
-                dfs(start, next, visited);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        // 입력
+    public static void main(String[] args) throws IOException{
+        //입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N][N];
-        res = new int[N][N];
-        
-        for (int i = 0; i < N; i++) {
+        for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<N; j++) arr[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int k=0; k<N; k++){
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    if(arr[i][k]==1 && arr[k][j]==1){
+                        arr[i][j] = 1;
+                    }
+                }
             }
         }
 
-        // DFS 수행
-        for (int i = 0; i < N; i++) {
-            boolean[] visited = new boolean[N];
-            dfs(i, i, visited);
-        }
-
-        // 출력
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(res[i][j] + " ");
+        //결과 출력
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
