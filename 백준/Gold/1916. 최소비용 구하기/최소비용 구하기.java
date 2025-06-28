a321c12b20ce044add28fad98e76e4471a0c0cc3
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-06-28 14:58:28 +0900

    [Gold V] Title: 최소비용 구하기, Time: 384 ms, Memory: 50456 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,64 +1,57 @@
 import java.io.*;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static int start, end; //출발 도시, 도착 도시
-    public static int N; //도시의 개수
-    public static int M; //버스의 개수
+    public static int N, M; //N=도시의 개수, M=버스의 개수
     public static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+    public static int start, end;
     static class Node{
         int idx;
-        int value;
-        public Node(int idx, int value) {
+        int cost;
+        public Node(int idx, int cost){
             this.idx = idx;
-            this.value = value;
+            this.cost = cost;
         }
     }
     public static void main(String[] args) throws IOException {
-        //입력
+        //입력부
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        for(int i=0; i<=N; i++){
-            graph.add(new ArrayList<>());
-        }
+        StringTokenizer st;
+        for(int i=0; i<=N; i++) graph.add(new ArrayList<>());
         for(int i=0; i<M; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             graph.get(s).add(new Node(e,v));
         }
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         start = Integer.parseInt(st.nextToken());
         end = Integer.parseInt(st.nextToken());
 
-        //다익스트라 초기화
         int dist[] = new int[N+1];
-        for(int i=0; i<N+1; i++) dist[i] = Integer.MAX_VALUE;
-
-        //큐 순회
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {return Integer.compare(o1.value,o2.value);});
-        pq.offer(new Node(start,0));
+        for(int i=1; i<=N; i++) dist[i] = Integer.MAX_VALUE;
         dist[start] = 0;
 
+        //PQ 순회 (다익스트라)
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)-> Integer.compare(a.cost,b.cost));
+        pq.offer(new Node(start, 0));
         while(!pq.isEmpty()){
             Node cur = pq.poll();
-            List<Node> adjList = graph.get(cur.idx);
-
             if(cur.idx == end) break;
-            if(dist[cur.idx]<cur.value) continue;
-
-            for(Node node : adjList){
-                if(dist[node.idx]>cur.value+node.value){
-                    dist[node.idx] = cur.value + node.value;
-                    pq.offer(new Node(node.idx, dist[node.idx]));
+            for(Node node : graph.get(cur.idx)){
+                int costSum = cur.cost + node.cost;
+                if(dist[node.idx] > costSum){
+                    dist[node.idx] = costSum;
+                    pq.offer(new Node(node.idx,costSum));
                 }
             }
         }
-
         System.out.println(dist[end]);
-
     }
+
 }
