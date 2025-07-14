c9920cccf294e4959ccb3d9d523d1dde8f71727d
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-07-14 10:51:15 +0900

    [level 3] Title: 입국심사, Time: 101.58 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java#) 
 
 ### 성능 요약
 
-메모리: 93.8 MB, 시간: 148.23 ms
+메모리: 88.4 MB, 시간: 101.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 28일 18:29:52
+2025년 07월 14일 10:51:12
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,28 +2,27 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        
+        //정렬 
         Arrays.sort(times);
-        long low = 1;
-        long high = (long)times[times.length-1] * n;
+        long answer = (long)n * times[times.length-1]; //가능한 최댓값 
         
-        while(low<=high){
-            long mid = (low+high)/2;
+        long left = 0; long right = answer;
+        while(left <= right){
+            long mid = (left+right)/2;
             
-            long cnt = 0; 
-            for(int time : times) {
-                cnt += mid/time;  
+            long cnt = 0;
+            for(int t : times){
+                cnt += mid/t;
             }
-           
-            if(cnt >= n){
+
+            if(cnt<n){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
                 answer = mid;
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
             }
         }
+        
         return answer;
     }
 }
