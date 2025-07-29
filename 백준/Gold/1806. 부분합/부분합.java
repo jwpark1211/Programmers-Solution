0b74044daa1f28c007753607c4f517c870cb6109
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-07-29 23:12:40 +0900

    [Gold IV] Title: 부분합, Time: 260 ms, Memory: 24052 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,33 +1,36 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static int N;
-    public static int S;
-    public static int ans = Integer.MAX_VALUE;
+    static int N;
+    static long S;
+    static int[] array;
+    static int answer = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-
-        int arr[] = new int[N];
+        S = Long.parseLong(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        array = new int[N];
+        for(int i=0; i<N; i++) array[i] = Integer.parseInt(st.nextToken());
+
+        long sum = array[0];
         int left = 0; int right = 0;
-        long sum = arr[left];
-        while(left<=right){
-            if(sum<S && right == N-1) break;
-            if(sum>=S) {
-                ans = Math.min(ans, right-left+1);
-                sum -= arr[left++];
-            }
-            else {
-                if (right < N - 1) sum += arr[++right];
+        while(right<N && left<=right){
+            if(sum >= S){
+                answer = Math.min(answer, right-left+1);
+                sum -= array[left++];
+            }else{
+                if(right==N-1) break;
+                sum += array[++right];
             }
         }
 
-        if(ans == Integer.MAX_VALUE) System.out.println(0);
-        else System.out.println(ans);
+        if(answer == Integer.MAX_VALUE) System.out.println(0);
+        else System.out.println(answer);
     }
 }
