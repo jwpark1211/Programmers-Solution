ea28bca4a57131ae62429560fba178f8bda147ac
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-07-30 11:42:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 464 ms, Memory: 47720 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,71 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    //edge
+    static int V,E; //V=정점의 개수, E=간선의 개수
+    static int[] parent;
+    static int distance = 0;
     static class Edge{
-        int n1;
-        int n2;
-        int value;
-        public Edge(int n1, int n2, int value){
-            this.n1 = n1;
-            this.n2 = n2;
-            this.value = value;
+        int e1;
+        int e2;
+        int cost;
+        public Edge(int e1, int e2, int cost){
+            this.e1 = e1;
+            this.e2 = e2;
+            this.cost = cost;
         }
     }
-    static int V,E; //V = 정점의 개수, E= 간선의 개수
-    static int[] parent ;
-    static int ans = 0;
     public static void main(String[] args) throws IOException {
-        //입력부
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-        Queue<Edge> q = new PriorityQueue<>((o1,o2)->Integer.compare(o1.value, o2.value));
-        for(int i=0; i<E; i++) {
+        parent = new int[V+1];
+        PriorityQueue<Edge> pq = new PriorityQueue<>((o1,o2)->Integer.compare(o1.cost, o2.cost));
+
+        for(int i=0; i<E; i++){
             st = new StringTokenizer(br.readLine());
-            int n1 = Integer.parseInt(st.nextToken());
-            int n2 = Integer.parseInt(st.nextToken());
-            int val = Integer.parseInt(st.nextToken());
-            q.offer(new Edge(n1,n2,val));
+            int e1 = Integer.parseInt(st.nextToken());
+            int e2 = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            pq.add(new Edge(e1,e2,cost));
         }
 
-        //union-find를 위한 parent 테이블 초기화
-        parent = new int[V+1];
         for(int i=1; i<=V; i++) parent[i] = i;
 
-        int cnt = 0;
-        while(cnt<V && !q.isEmpty()){
-            Edge cur = q.poll();
-            if(findParent(cur.n1,cur.n2)) continue;
-            unionParent(cur.n1, cur.n2);
-            cnt ++;
-            ans += cur.value;
+
+        while(!pq.isEmpty()){
+            Edge cur = pq.poll();
+            if(!findParent(cur.e1,cur.e2)){
+                distance += cur.cost;
+                unionParent(cur.e1, cur.e2);
+            }
         }
 
-        System.out.println(ans);
+        System.out.println(distance);
+
     }
-    //부모를 찾음
-    static int getParent(int x){
+    public static int getParent(int x){
         if(parent[x]==x) return x;
         return parent[x] = getParent(parent[x]);
     }
-    //두 노드의 부모를 합침
-    static void unionParent(int a, int b){
+
+    public static void unionParent(int a, int b){
         a = getParent(a);
         b = getParent(b);
-        if(a < b) parent[b] = a;
+        if(a<b) parent[b] = a;
         else parent[a] = b;
     }
-    //같은 부모를 가지는지 확인
-    static boolean findParent(int a, int b){
+
+    public static boolean findParent(int a, int b){
         a = getParent(a);
         b = getParent(b);
         return a==b;
     }
+
 }
