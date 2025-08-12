a9873696cb4da82a6962b6d670b6c8fd5382a6e5
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-08-12 13:52:27 +0900

    [Gold V] Title: 퇴사 2, Time: 660 ms, Memory: 310260 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,38 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
-    static int dp[] ;
-    static int Ti[];
-    static int Pi[];
-    static int answer = 0;
     public static void main(String[] args) throws IOException {
+        //입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        dp = new int[N+1];
-        Ti = new int[N+1];
-        Pi = new int[N+1];
-
-        for(int i=1; i<=N; i++){
+        int time[] = new int[N+1];
+        int profit[] = new int[N+1];
+        for(int i=1; i<N+1; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            Ti[i] = Integer.parseInt(st.nextToken());
-            Pi[i] = Integer.parseInt(st.nextToken());
+            time[i] = Integer.parseInt(st.nextToken());
+            profit[i] = Integer.parseInt(st.nextToken());
         }
 
+        int dp[] = new int[N+1];
         for(int i=1; i<=N; i++){
-            if(i+Ti[i]-1<=N){
-                dp[i+Ti[i]-1] = Math.max(dp[i+Ti[i]-1],Pi[i]+dp[i-1]);
-            }
             dp[i] = Math.max(dp[i],dp[i-1]);
-            answer = Math.max(dp[i], answer);
+            if(i+time[i]-1<=N) {
+                dp[i + time[i] - 1] = Math.max(dp[i + time[i] - 1], profit[i] + dp[i-1]);
+            }
         }
-
-        //결과 출력
-        System.out.println(answer);
+        System.out.println(dp[N]);
     }
-
 }
