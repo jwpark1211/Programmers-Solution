05dabf56c22b6c676125a529b5c1f200db454f79
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-08-20 12:48:33 +0900

    [Gold III] Title: 소형기관차, Time: 192 ms, Memory: 19784 KB -BaekjoonHub

--- 백준/Gold/2616. 소형기관차/소형기관차.java
+++ 백준/Gold/2616. 소형기관차/소형기관차.java
@@ -1,32 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
 public class Main {
-
+    static int total; //전체 객차 수
+    static int[] people;
+    static int max; //한 소형 기관차가 끌 수 있는 객차 수
     public static void main(String[] args) throws IOException {
-        //입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int trainNum = Integer.parseInt(br.readLine());
+        total = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int person[] = new int[trainNum+1]; //35 40 50 10 30 45 60
-        for(int i=1; i<=trainNum; i++) person[i] = Integer.parseInt(st.nextToken());
-        //누적합 계산
-        int sum[] = new int[trainNum+1];
-        for(int i=1; i<=trainNum; i++) sum[i]=sum[i-1]+person[i];
-        //한 소형차에 달릴 객차의 수
-        int M = Integer.parseInt(br.readLine());
+        people = new int[total+1];
+        for(int i=1; i<=total; i++) people[i] = Integer.parseInt(st.nextToken());
+        max = Integer.parseInt(br.readLine());
 
-        int dp[][] = new int[4][trainNum+1];
-        for(int i=1; i<4; i++){
-            for(int j=M*i; j<=trainNum; j++){
-                dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j-M]+(sum[j]-sum[j-M]));
+        int[] sum = new int[total+1];
+        for(int i=1; i<=total; i++) sum[i]=sum[i-1]+people[i];
+
+        int dp[][] = new int[4][total+1];
+        for(int i=1; i<=3; i++){
+            for(int j=max*i; j<=total; j++){
+                dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j-max]+(sum[j]-sum[j-max]));
             }
         }
-
-        System.out.println(dp[3][trainNum]);
+        System.out.println(dp[3][total]);
     }
 }
