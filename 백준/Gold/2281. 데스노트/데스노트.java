9788b4c295559942c794b2736b22e29e4c55db5d
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-08-20 12:14:03 +0900

    [Gold III] Title: 데스노트, Time: 120 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/2281. 데스노트/데스노트.java
+++ 백준/Gold/2281. 데스노트/데스노트.java
@@ -1,43 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int names[] = new int[1001];
-    static int n, m;
+    static int n,m;
+    static int names[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        for(int i=0; i<n; i++) names[i] = Integer.parseInt(br.readLine());
+        names = new int[n+1];
+        for(int i=1; i<=n; i++) names[i] = Integer.parseInt(br.readLine());
+
+        int sumLen[] = new int[n+1];
+        sumLen[1] = names[1];
+        for(int i=2; i<=n; i++) sumLen[i] = sumLen[i-1]+names[i];
+        //System.out.println(Arrays.toString(sumLen));
 
         int dp[] = new int[n+1];
-        for(int i=1; i<=n; i++) dp[i] = Integer.MAX_VALUE;
-        dp[0] =0;
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
-                int lineCnt = canOneLine(j, i);
-                if(lineCnt==-1) continue;
-                if(i==n){
-                    dp[i] = Math.min(dp[i], dp[j-1]);
-                }else {
-                    dp[i] = Math.min(dp[i], dp[j - 1] + (m - lineCnt) * (m - lineCnt));
+            for(int j=0; j<i; j++){
+                int len = sumLen[i]-sumLen[j]+(i-j-1);
+                if(len<=m){
+                    if(i==n) dp[i] = Math.min(dp[i],dp[j]);
+                    else dp[i] = Math.min(dp[i], dp[j]+(m-len)*(m-len));
                 }
             }
         }
         System.out.println(dp[n]);
-    }
-
-    public static int canOneLine(int startIdx, int endIdx){
-        int cnt = 0;
-        for(int i=startIdx; i<=endIdx; i++){
-            cnt += names[i-1];
-            if(i!=endIdx) cnt += 1;
-        }
-        if(cnt<=m) return cnt;
-        return -1;
     }
 }
