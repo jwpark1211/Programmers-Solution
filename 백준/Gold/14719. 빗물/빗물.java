4ac3e8817bd64269d46723a666f481175593fff4
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-08-21 12:33:30 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,35 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int H,W; //(1 ≤ H, W ≤ 500)
-    static int[] block;
+public class Main{
+    static int H,W;
+    static int block[];
     static int answer = 0;
     public static void main(String[] args) throws IOException {
-        // 입력
-        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        //입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         H = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
+        block = new int[W+1];
         st = new StringTokenizer(br.readLine());
-        block = new int[W];
-        for(int i=0; i<W; i++) block[i] = Integer.parseInt(st.nextToken());
-
+        for(int i=1; i<=W; i++) block[i]=Integer.parseInt(st.nextToken());
         //계산
-        int left = block[0];
-        for(int i=1; i<W; i++){
-            //left max
-            left = Math.max(left, block[i-1]);
-            //right max
-            int right = 0;
-            for(int j=i+1; j<W; j++) right = Math.max(right, block[j]);
-
-            int min = Math.min(left, right);
-            if(block[i]<min) answer += (min - block[i]);
+        int left = block[1];
+        int right = 0;
+        for(int i=2; i<W; i++){
+            left = Math.max(block[i-1],left);
+            right = 0;
+            for(int j=i+1; j<=W; j++) right=Math.max(right,block[j]);
+            int b = Math.min(left,right);
+            if(b>block[i]) answer += b-block[i];
         }
-
         System.out.println(answer);
     }
 }
