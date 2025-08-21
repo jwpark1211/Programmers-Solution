6784e697e0f4433899fe3cb0292aba7411dd71b5
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-08-21 11:23:20 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,56 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
-    static int[] num;
-    static int[] op = new int[4]; // 차례로 +, - , *, %
+    static int num[];
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
-
-    static void dfs(int idx, int sum){
-        if(idx==N-1){
-            max = Math.max(sum, max);
-            min = Math.min(sum, min);
+    public static void dfs(int idx, int sum, int plus, int minus, int mul, int div) {
+        if(idx==N){
+            max = Math.max(max, sum);
+            min = Math.min(min, sum);
             return;
-        }
-
-        if(op[0]>0) {
-            op[0]--;
-            dfs(idx+1,sum + num[idx+1]);
-            op[0]++;
-        }
-        if(op[1]>0) {
-            op[1]--;
-            dfs(idx+1,sum - num[idx+1]);
-            op[1]++;
-        }if(op[2]>0) {
-            op[2]--;
-            dfs(idx+1,sum * num[idx+1]);
-            op[2]++;
-        }if(op[3]>0 && num[idx+1]!=0) {
-            op[3]--;
-            if(sum<0 && num[idx+1]>0) dfs(idx+1, (Math.abs(sum) / num[idx+1])*-1);
-            else dfs(idx+1,sum / num[idx+1]);
-            op[3]++;
+        }else{
+            if(plus>0){
+                dfs(idx+1, sum+num[idx],plus-1, minus, mul, div);
+            }
+            if(minus>0){
+                dfs(idx+1, sum-num[idx], plus, minus-1, mul, div);
+            }
+            if(mul>0){
+                dfs(idx+1, sum*num[idx], plus, minus, mul-1, div);
+            }
+            if(div>0){
+                if(sum<0){
+                    dfs(idx+1, -1*((-1*sum)/num[idx]), plus, minus, mul,div-1);
+                }else {
+                    dfs(idx+1, sum/num[idx], plus, minus, mul, div-1);
+                }
+            }
         }
     }
     public static void main(String[] args) throws IOException {
-        //입력부
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         num = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) num[i] = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<4; i++) op[i] = Integer.parseInt(st.nextToken());
-
-        //dfs
-        dfs(0, num[0]);
-
-        //결과
+        int plus = Integer.parseInt(st.nextToken());
+        int minus = Integer.parseInt(st.nextToken());
+        int mul = Integer.parseInt(st.nextToken());
+        int div = Integer.parseInt(st.nextToken());
+        dfs(1,num[0],plus,minus,mul,div);
         System.out.println(max);
         System.out.println(min);
     }
