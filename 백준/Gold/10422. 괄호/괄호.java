170d5bc96a4f5aa548b3c376edd98a59af99843b
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-08-22 11:12:18 +0900

    [Gold IV] Title: 괄호, Time: 168 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/10422. 괄호/괄호.java
+++ 백준/Gold/10422. 괄호/괄호.java
@@ -1,35 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
 
-
-public class Main {
-
+public class Main{
+    static int mod = 1_000_000_007;
+    static int T;
+    static int L[];
     public static void main(String[] args) throws IOException {
-        //입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int dp[][] = new int[5001][5001];
-        dp[2][1] = 1;
-        for(int i=4; i<=5000; i+=2){
-            for(int j=i/2; j>0; j--){
-                dp[i][j]= (dp[i][j+1]+dp[i-2][j-1])%1000000007;
-            }
-        }
+        T = Integer.parseInt(br.readLine());
+        L = new int[T];
+        for(int i=0; i<T; i++) L[i] = Integer.parseInt(br.readLine());
 
-        /*for(int i=0; i<=10; i++){
-            for(int j=0; j<=10; j++){
-                System.out.print(dp[i][j]+" ");
+        long dp[] = new long[5001];
+        dp[0]=1; dp[2]=1;
+
+        for(int i=4; i<=5000; i+=2){
+            for(int j=0; j<i; j+=2){
+                dp[i]+=(dp[j]*dp[i-j-2])%mod;
+                dp[i]%=mod;
             }
-            System.out.println();
-        }*/
-        for(int i=0; i<T; i++) {
-            int key = Integer.parseInt(br.readLine());
-            long sum = 0;
-            for (int j = 0; j <= key/2+1; j++) sum = (sum + dp[key][j])%1000000007;
-            System.out.println(sum);
         }
+        for(int l : L) System.out.println(dp[l]);
     }
 }
