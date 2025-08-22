e450f27e2fd85b6d5a6bcfdda8998a8f650f8eb3
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-08-22 12:36:06 +0900

    [Gold III] Title: 줄 세우기, Time: 800 ms, Memory: 54860 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,40 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M; //사람수, 간선수
-    static List<ArrayList<Integer>> list = new ArrayList<>(); //그래프
-    static int[] in; //진입차수
-    static List<Integer> ans = new ArrayList<>();
+    static int M,N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        for(int i=0; i<=N; i++) list.add(new ArrayList<>());
-        in = new int[N+1]; in[0] = -1;
+        int fIn[] = new int[N+1];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0; i<=N; i++) graph.add(new ArrayList<>());
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            list.get(x).add(y);
-            in[y]++;
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            graph.get(s).add(e);
+            fIn[e]++;
         }
 
         Queue<Integer> q = new LinkedList<>();
-        for(int i=1; i<=N; i++) if(in[i]==0) q.add(i);
+        for(int i=1; i<=N; i++){
+            if(fIn[i]==0) q.offer(i);
+        }
+
         while(!q.isEmpty()){
-            int x = q.poll();
-            ans.add(x);
-            List<Integer> tmp = list.get(x);
-            for(int i=0; i<tmp.size();i++){
-                in[tmp.get(i)]--;
-                if(in[tmp.get(i)]==0) q.add(tmp.get(i));
+            int cur = q.poll();
+            System.out.print(cur+" ");
+
+            for(int i : graph.get(cur)){
+                fIn[i]--;
+                if(fIn[i]==0){
+                    q.offer(i);
+                }
             }
         }
-        for(int i : ans) System.out.print(i + " ");
     }
 }
