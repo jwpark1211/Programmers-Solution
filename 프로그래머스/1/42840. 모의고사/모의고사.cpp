edabcf76625d55e18b4fadccbe5acce6efc477e1
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-08-28 12:07:25 +0900

    [level 1] Title: 모의고사, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,9 +22,12 @@ vector<int> solution(vector<int> answers) {
         if(man3[i%man3.size()]==answers[i]) cnt3++;
     }
     
-    if(cnt1>=cnt2 && cnt1>=cnt3) answer.push_back(1);
-    if(cnt2>=cnt3 && cnt2>=cnt1) answer.push_back(2);
-    if(cnt3>=cnt2 && cnt3>=cnt1) answer.push_back(3);
+    vector<int> v {cnt1,cnt2,cnt3};
+    int final_max = *max_element(v.begin(), v.end());
+    if(cnt1==final_max) answer.push_back(1);
+    if(cnt2==final_max) answer.push_back(2);
+    if(cnt3==final_max) answer.push_back(3);
+
 
     
     return answer;
