4b034052d9b94d19b4b4c3df03630d2cd821b861
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-09-03 11:04:58 +0900

    [level 2] Title: 더 맵게, Time: 212.46 ms, Memory: 45.3 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=cpp#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 45.3 MB, 시간: 242.60 ms
+메모리: 45.3 MB, 시간: 212.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 28일 17:04:33
+2025년 09월 03일 11:04:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -2,32 +2,23 @@
 #include <vector>
 #include <queue>
 #include <functional>
-#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-    int answer = 0;
-    priority_queue<long long, vector<long long>,greater<long long>> pq;
-    for(int i=0; i<scoville.size(); i++) pq.push(scoville[i]);
+    int ans = 0; //섞은 횟수 
     
-    while(true){
-        //모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우 
-        if(pq.size()==1 && pq.top()<K){
-            answer = -1;
-            break;
-        }
-        //가장 앞에 있는 수가 K가 넘으면 return 
-        if(pq.top()>=K) break;
-        
-        //default
-        long long tmp = pq.top();
-        pq.pop();
-        tmp+= pq.top()*2;
-        pq.pop();
-        pq.push(tmp);
-        answer++;
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+    for(int s : scoville) pq.push(s);
+    
+    while(pq.size()!=1){
+        if(pq.top()>=K) break; //가장 작은 게 K보다 크면 반복문 out
+        long long front = pq.top(); pq.pop();
+        long long second = pq.top(); pq.pop();
+        pq.push(front + second*2);
+        ans++;
     }
     
-    return answer;
+    if(pq.size()==1 && pq.top()<K) return -1;
+    return ans;
 }
