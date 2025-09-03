a6a2fb395315250cc89d966b99992694c9ed47c5
jeong woon <83337542+jwpark1211@users.noreply.github.com>
2025-09-03 12:10:22 +0900

    [level 2] Title: 배달, Time: 0.27 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.43 ms
+메모리: 4.21 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 29일 17:43:47
+2025년 09월 03일 12:10:20
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -6,44 +6,39 @@ using namespace std;
 struct Node{
     int idx;
     int cost;
-    
     bool operator<(const Node& other) const{
         return cost>other.cost;
     }
 };
-int solution(int N, vector<vector<int> > road, int K) {
+int solution(int N, vector<vector<int>> road, int K) {
     int answer = 0;
     vector<vector<Node>> graph(N+1);
-    
-    for(int i=0; i<road.size(); i++){
-        graph.at(road[i][0]).push_back({road[i][1],road[i][2]});
-        graph.at(road[i][1]).push_back({road[i][0],road[i][2]});
+    for(vector<int> r : road){
+        graph[r[0]].push_back({r[1],r[2]});
+        graph[r[1]].push_back({r[0],r[2]});
     }
     
-    vector<int> dist(N+1, 123456789);
-    dist[1]=0;
-    priority_queue<Node> q;
-    q.push({1,0});
+    vector<int> dist (N+1, 123456789);
+    dist[1] = 0;
     
-    while(!q.empty()){
-        Node cur = q.top();
-        q.pop();
+    priority_queue<Node> pq;
+    pq.push({1, 0}); //시작점 
+    
+    while(!pq.empty()){
+        Node cur = pq.top();
+        pq.pop();
         
         if(dist[cur.idx]<cur.cost) continue;
         
-        for(Node newNode : graph.at(cur.idx)){
-            if(newNode.idx!=1 && dist[newNode.idx]>cur.cost+newNode.cost){
-                dist[newNode.idx]=cur.cost+newNode.cost;
-                q.push({newNode.idx, dist[newNode.idx]});
+        for(Node nxt : graph[cur.idx]){
+            if(dist[nxt.idx]>nxt.cost+dist[cur.idx]){
+                dist[nxt.idx]=nxt.cost+dist[cur.idx];
+                pq.push({nxt.idx, dist[nxt.idx]});
             }
         }
     }
     
-    for(int i=1; i<N+1; i++){
-        if(dist[i]<=K) answer++;
-    }
-    for(int d : dist)
-        cout<<"d:"<<d<<endl;
-
+    for(int i=1; i<=N; i++) if(dist[i]<=K) answer++;
+    
     return answer;
 }
